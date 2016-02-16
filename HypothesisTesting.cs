12e205613fbcf1e4c2c0193ee97bc26f7207b9b0
JoshuaHe2015 <JoshuaHe2015@163.com>
2016-02-16 17:02:12 +0800

    add

---
+++ HypothesisTesting.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VISAP商科应用
+{
+    public partial class HypothesisTesting : Form
+    {
+        public static string result = "";
+        public HypothesisTesting()
+        {
+            InitializeComponent();
+            Tabulation.RenewColsItems(MainForm.MainDT, comboBox_Cols);
+        }
+
+        private void button_StartTesting_Click(object sender, EventArgs e)
+        {
+            result = Stat.HypothesisTesting(MainForm.MainDT, comboBox_Cols.Text,
+                comboBox_Statistics.Text, comboBox_Operation.Text, comboBox_Tail.Text,
+                Convert.ToDouble(comboBox_Significance.Text), Convert.ToDouble(textBox_NullHypothesis.Text));
+            MainForm.S.richTextBox1.AppendText(result);
+        }
+
+        private void comboBox_Statistics_TextChanged(object sender, EventArgs e)
+        {
+            string Operation = comboBox_Operation.Text;
+            if (Operation == "=")
+            {
+                Operation = "<>";
+            }
+            else if (Operation == "<=")
+            {
+                Operation = ">";
+            }
+            else
+            {
+                Operation = "<";
+            }
+            label_Alternative.Text = comboBox_Statistics.Text + " " + Operation + " " + textBox_NullHypothesis.Text;
+        }
+
+        private void comboBox_Operation_TextChanged(object sender, EventArgs e)
+        {
+            string Operation = comboBox_Operation.Text;
+            if (Operation == "=")
+            {
+                Operation = "<>";
+            }
+            else if (Operation == "<=")
+            {
+                Operation = ">";
+            }
+            else
+            {
+                Operation = "<";
+            }
+            label_Alternative.Text = comboBox_Statistics.Text + " " + Operation + " " + textBox_NullHypothesis.Text;
+        }
+
+        private void textBox_NullHypothesis_TextChanged(object sender, EventArgs e)
+        {
+            string Operation = comboBox_Operation.Text;
+            if (Operation == "=")
+            {
+                Operation = "<>";
+            }
+            else if (Operation == "<=")
+            {
+                Operation =">";
+            }
+            else
+            {
+                Operation ="<";
+            }
+            label_Alternative.Text = comboBox_Statistics.Text + " " + Operation + " " + textBox_NullHypothesis.Text;
+        }
+    }
+}
